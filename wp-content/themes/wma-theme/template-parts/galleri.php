7a7e65954b5dfdfd1a7dcc16a227b82c8e8afc9d
Stefan Eriksson Roskvist <stefan.erikssonrosqvist@medieinstitutet.se>
2018-01-26 12:59:58 +0100

    sixth
    more changes in gallery

--- wp-content/themes/wma-theme/template-parts/galleri.php
+++ wp-content/themes/wma-theme/template-parts/galleri.php
@@ -4,86 +4,57 @@ Template Name: galleri
 */
 get_header();?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
-	<?php wp_head(); ?>
-</head>
-<body>
-<header>
-
 	<div class="wma-textruta">
 		<h4 class="toptext">Galleri</h4>
 
-        <div class="gallery">
-            <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/inferno.jpg"/>
-            <div class="desc"><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Dreams )->ID ); ?>">Dreams of Summer</a></h2></div>
-        </div>
+        <div class="galleri">
+            <div class="grid">
+                <div class="cell">
+                    <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/dreamsofsummer.jpg"/>
+                    <figure><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Dreams )->ID ); ?>">Dreams of Summer</a></h2></figure>
+                </div>
+                <div class="cell">
+                    <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/inferno.jpg"/>
+                    <figure><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Inferno )->ID ); ?>">Inferno</a></h2></figure>
+                </div>
+                <div class="cell">
+                    <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/modern.jpg"/>
+                    <figure><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Modern )->ID ); ?>">Modern</a></h2></figure>
+                </div>
+                <div class="cell">
+                    <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/purplehaze.jpg"/>
+                    <figure><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Purple )->ID ); ?>">Purple Haze</a></h2></figure>
+                </div>
+                <div class="cell">
+                    <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/inferno.jpg"/>
+                    <figure><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Slayer )->ID ); ?>">Slayer</a></h2></figure>
+                </div>
+                <div class="cell">
+                    <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/spilledcoffee.jpg"/>
+                    <figure><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Spilled )->ID ); ?>">Spilled Coffee</a></h2></figure>
+                </div>
+                <div class="cell">
+                    <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/trappa.jpg"/>
+                    <figure><div class="desc"><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Stairway )->ID ); ?>">Stairway</a></h2></figure>
+                </div>
+                <div class="cell">
+                    <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/sverige.jpg"/>
+                    <figure><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Sverige )->ID ); ?>">Sverige</a></h2></figure>
+                </div>
+                <div class="cell">
+                    <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/vatten.jpg"/>
+                    <figure><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Vatten )->ID ); ?>">Vatten</a></h2></figure>
+                </div>
+                <div class="cell">
+                    <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/words.jpg"/>
+                    <figure><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Words )->ID ); ?>">Words</a></h2></figure>
+                </div>
 
-        <div class="gallery">
-            <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/inferno.jpg"/>
-            <div class="desc"><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Inferno )->ID ); ?>">Inferno</a></h2></div>
-        </div>
-
-        <div class="gallery">
-            <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/inferno.jpg"/>
-            <div class="desc"><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Modern )->ID ); ?>">Modern</a></h2></div>
-        </div>
-
-        <div class="gallery">
-            <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/inferno.jpg"/>
-            <div class="desc"><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Purple )->ID ); ?>">Purple Haze</a></h2></div>
-        </div>
-
-        <div class="gallery">
-            <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/inferno.jpg"/>
-            <div class="desc"><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Slayer )->ID ); ?>">Slayer</a></h2></div>
-        </div>
-
-        <div class="gallery">
-            <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/inferno.jpg"/>
-            <div class="desc"><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Spilled )->ID ); ?>">Spilled Coffee</a></h2></div>
-        </div>
-
-        <div class="gallery">
-            <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/inferno.jpg"/>
-            <div class="desc"><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Stairway )->ID ); ?>">Stairway</a></h2></div>
-        </div>
-
-        <div class="gallery">
-            <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/inferno.jpg"/>
-            <div class="desc"><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Sugar )->ID ); ?>">Sugar Love</a></h2></div>
-        </div>
-
-        <div class="gallery">
-            <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/inferno.jpg"/>
-            <div class="desc"><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Sverige )->ID ); ?>">Sverige</a></h2></div>
-        </div>
-
-        <div class="gallery">
-            <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/inferno.jpg"/>
-            <div class="desc"><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Vatten )->ID ); ?>">Vatten</a></h2></div>
-        </div>
-
-        <div class="gallery">
-            <img class="galleri-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/inferno.jpg"/>
-            <div class="desc"><h2 class="galleri-title"><a href="<?php echo get_page_link( get_page_by_title( Words )->ID ); ?>">Words</a></h2></div>
-        </div>
-
-
-
-
-
-
-
-
-
+            </div> <!-- grid -->
+        </div> <!-- galleri -->
 
 
 	</div> <!-- wma-textruta -->
-
-	</body>
 
 
 <?php get_footer();
