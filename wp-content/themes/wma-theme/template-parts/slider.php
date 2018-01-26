0deafdf17d706d96ca52952034001eee746b52f5
Stefan Eriksson Roskvist <stefan.erikssonrosqvist@medieinstitutet.se>
2018-01-26 10:29:47 +0100

    fourth
    småfix

--- wp-content/themes/wma-theme/template-parts/slider.php
+++ wp-content/themes/wma-theme/template-parts/slider.php
@@ -1,13 +1,13 @@
 <?php ?>
 
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 
- <div class="slideShow">
+<div class="slideShow">
     <img class="placeHolder" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/img-slide1.jpg">
     <div class="wmalogo"><img src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/wma-logo.png"></div>
     <div class="layer1"></div>
