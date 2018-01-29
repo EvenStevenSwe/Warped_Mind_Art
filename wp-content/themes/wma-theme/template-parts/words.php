7fa00d620746586831f953ab9668f7b8da6bba6c
Stefan Eriksson Roskvist <stefan.erikssonrosqvist@medieinstitutet.se>
2018-01-29 13:46:13 +0100

    eleventh
    infosidor om alla tavlor

--- wp-content/themes/wma-theme/template-parts/words.php
+++ wp-content/themes/wma-theme/template-parts/words.php
@@ -4,8 +4,29 @@ Template Name: words
 */
 get_header();?>
 
-	<div class="artinfo">
+    <div class="wma-textruta">
+        <h4 class="toptext">Words</h4>
 
-	</div><!-- artinfo -->
+        <div class="wma-text">
+
+            <div class="artinfo left">
+                <img class="artinfo-img" src="<?php echo $theme; ?>/wp-content/themes/wma-theme/img/words.jpg"/>
+
+            </div> <!-- artinfo left -->
+
+            <div class="artinfo right">
+
+                <p>Gjord på canvas med akrylfärger</p>
+                50 x 40 cm.
+
+
+            </div> <!-- artinfo right -->
+
+        </div> <!-- wma-text -->
+        <div class="artinfo-back">
+            <h2>Tillbaka till <a href="javascript:history.go(-1)">Galleriet</a></h2>
+        </div> <!-- artinfo-back -->
+
+    </div> <!-- wma-textruta -->
 
 <?php get_footer();
