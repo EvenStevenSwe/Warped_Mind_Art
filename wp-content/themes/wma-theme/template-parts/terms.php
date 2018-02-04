0f9166865ddefdea787689b606758d77a7a39acc
Stefan Eriksson Roskvist <stefan.erikssonrosqvist@medieinstitutet.se>
2018-02-04 21:47:05 +0100

    eighteenth
    småfix.

--- wp-content/themes/wma-theme/template-parts/terms.php
+++ wp-content/themes/wma-theme/template-parts/terms.php
@@ -5,14 +5,27 @@ Template Name: terms
 get_header();?>
 
 	<div class="wma-textruta">
-		<h4 class="toptext">Köp & Leverans</h4>
+		<h4 class="toptext">Köp & Leveransvillkor</h4>
 
 		<div class="wma-text">
 			<h2>
 
+                <p>1. Ditt köp på Warped Mind Art<br>
+                Efter avslutat köp kommer du få sammanfattning av din beställning samt kvitto till den e-post adress du har uppgett.</p>
 
+                <p>2. Betalsätt<br>
+                Jag tillämpar enbart direkta betalningar genom betrodda betalningsleverantörer som t.ex Klarna, Paypal och Stripe.</p>
 
+                <p>3. Leverans<br>
+                Beställda varor skickas väl förpackat för att undkomma ev. fraktskador med den leverantör som passar bäst för uppgiften.</p>
 
+                <p>4. Avbeställning/Retur<br>
+                    <sup>4.1</sup> Skulle du ångra ditt köp innan jag hunnit skicka din beställning är det att du skriver till mig så är det inga problem.<br>
+                    <sup>4.2</sup> Önskar du att returnera en vara ber jag dig kontakta mig.</p>
+
+                <p>5. Garantier<br>
+                Jag kan inte lämna någon garanti på redan levererade varor. Så vidare dom inte går sönder på väg till dig förutsätter jag
+                att du behandlar ditt köp på ett sätt som gör att det inte blir skadat.</p>
 
 			</h2>
 		</div> <!-- wma-text -->
